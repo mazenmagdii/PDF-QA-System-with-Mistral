5c0b48d9fece67740de7274ef61a4bbe7e3a0103
Mazen Magdi <mazenmagdii2022@gmail.com>
2026-01-18 19:30:51 +0200

    Delete src directory

--- src/pdf_loader.py
+++
@@ -1,8 +0,0 @@
-from PyPDF2 import PdfReader
-
-def load_pdf(pdf_path):
-    reader = PdfReader(pdf_path)
-    full_text = ""
-    for page in reader.pages:
-        full_text += page.extract_text() + "\n"
-    return full_text
--- src/vector_store.py
+++
@@ -1,8 +0,0 @@
-import faiss
-
-def create_faiss_index(embeddings):
-    dim=embeddings.shape[1]
-    index=faiss.IndexFlatL2(dim)
-    index.add(embeddings)
-    return index
-
